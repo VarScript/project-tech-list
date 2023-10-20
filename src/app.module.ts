b88eaddd7e76d8b3a7b638b2b8c068479b4a83cc
VarScript <vargassanchezfabio01@gmail.com>
2023-10-19 23:10:41 -0500

    connect database with typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,21 @@
+import { join } from 'path';
 import {
   ApolloDriver,
   ApolloDriverConfig,
 } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
-import { join } from 'path';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
+
 import { ItemsModule } from './items/items.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
+
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       // debug: false,
@@ -19,6 +25,18 @@ import { ItemsModule } from './items/items.module';
         ApolloServerPluginLandingPageLocalDefault(),
       ],
     }),
+
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      synchronize: true,
+      autoLoadEntities: true
+    }),
+
     ItemsModule,
   ],
   controllers: [],
