dbf3aaa820241f5cd4f2f1d7615604f2bc5631a9
varscript <vargassanchezfabio01@gmail.com>
2023-11-23 15:41:52 -0500

    protect seed

--- src/seed/seed.module.ts
+++ src/seed/seed.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { SeedService } from './seed.service';
 import { SeedResolver } from './seed.resolver';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   providers: [SeedResolver, SeedService],
-  imports: [TypeOrmModule]
+  imports: [TypeOrmModule, ConfigModule],
 })
 export class SeedModule {}
--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,10 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class SeedService {
-  constructor() {}
+  private isProd: boolean;
+
+  constructor(
+    private readonly configService: ConfigService,
+  ) {
+    this.isProd = configService.get('STATE') === 'prod';
+  }
 
   async executeSeed(): Promise<boolean> {
+    if (this.isProd) {
+      throw new UnauthorizedException(
+        'We cannot run SEED on production',
+      );
+    }
     // Clean DB
 
     // Create Users
