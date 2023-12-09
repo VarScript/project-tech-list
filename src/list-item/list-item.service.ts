5b01815dd18f5d7c846ab3db3b9093412e26a4fc
varscript <vargassanchezfabio01@gmail.com>
2023-12-09 17:16:40 -0500

    error create listitem

--- src/list-item/list-item.service.ts
+++ src/list-item/list-item.service.ts
@@ -32,7 +32,9 @@ export class ListItemService {
       list: { id: listId },
     });
 
-    return this.listItemRepository.save(newListItem);
+    await this.listItemRepository.save(newListItem);
+
+    return this.findOne(newListItem.id);
   }
 
   async findAll(
